e09977e82a5f8535386ba468582a769436f0f4d3
Rashedul Haydar <rh2712@columbia.edu>
2021-01-12 22:59:10 -0500

    Ch. 12 and 13: Add FAB and snackbar to order activity.
    For chapter 13, partially done with adding a recycler view
    to be able to show cards for each pizza instead of a plain
    list.

--- app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
@@ -1,10 +1,14 @@
 package com.hfad.bitsandpizzas;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.os.Bundle;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -16,5 +20,20 @@ public class OrderActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickDone(View view) {
+        CharSequence text = "Your order has been updated";
+        int duration = Snackbar.LENGTH_SHORT;
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator), text, duration);
+        snackbar.setAction("Undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(OrderActivity.this, "Undone!",
+                        Toast.LENGTH_SHORT);
+                toast.show();;
+            }
+        });
+        snackbar.show();
     }
 }
